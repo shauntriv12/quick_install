a2647c5d150ca87757b973248129e8d918e82dee
shaun <shaun@roemin.com>
2015-03-26 11:40:57 +1000

    lamp_stack uninstallation
    It uninstalls lamp_stack i.e. apache server, mysql server and php on a
    linux ubuntu server.

---
+++ apache_server_uninstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Apache server uninstallation
+
+sudo apt-get -y remove apache2
+
+#MySQL uninstallation 
+
+sudo apt-get -y remove mysql-server
+
+#php uninstallation
+
+sudo apt-get -y remove php5 libapache2-mod-php5 php5-mcrypt
+
+
+
